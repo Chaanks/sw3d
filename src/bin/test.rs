1dd388756baf436d6bd7002d1fd3d4cad5a6a2db
Jarod <jarod8405@gmail.com>
2018-10-14 02:39:43 +0200

    end window implementation

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,7 +1,19 @@
 extern crate sw3d;
 
+extern crate winit;
+
+
 
 fn main () {
     println!("ez");
-    sw3d::window::Window::new(800, 600, "Test");
+    let mut window = sw3d::window::Window::new(800, 600, "Test");
+    window.events_loop.run_forever(|event| {
+        match event {
+            winit::Event::WindowEvent { event: winit::WindowEvent::CloseRequested, .. } => {
+                winit::ControlFlow::Break
+            },
+
+            _ => winit::ControlFlow::Continue,
+        }
+    });
 }
